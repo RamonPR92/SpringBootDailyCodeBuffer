5bea6f28dcda3293de0d2d4779361c2822772a11
RamonBEA <ramon.perez@sistemabea.mx>
2023-06-03 11:56:17 -0600

    Actuator module
    In this module we saw how configure the app to expose applications' endpoints that show operational information when we use /actuator path, and also we saw how make our endpoints and show personalized information, and we saw how include and exclude endpoints

---
+++ src/main/java/com/ramon/apirestdemo/config/FeatureEndpoint.java
@@ -0,0 +1,44 @@
+package com.ramon.apirestdemo.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+//We can do endpoints for expose information when we call the path /actuator
+@Component
+@Endpoint(id = "features")
+public class FeatureEndpoint {
+
+    private final Map<String,Feature> featureMap =
+            new ConcurrentHashMap<>();
+
+    public FeatureEndpoint() {
+        featureMap.put("Department", new Feature(true));
+        featureMap.put("User", new Feature(false));
+        featureMap.put("Authentication", new Feature(false));
+    }
+
+    @ReadOperation
+    public Map<String, Feature> getFeatures(){
+        return featureMap;
+    }
+
+    @ReadOperation
+    public Feature getFeature(@Selector String featureName){
+        return featureMap.get(featureName);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class Feature{
+        private boolean isEnable;
+    }
+}
